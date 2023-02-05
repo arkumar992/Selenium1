117073921c52b92728ec16480b9cf38cd66bc1d2
Arun <Arun@Arun-PC>
2023-02-05 21:30:01 +0530

    Test

---
+++ src/main/java/selflearnjavaPackage/SwichCaseExample.java
@@ -0,0 +1,41 @@
+package selflearnjavaPackage;
+
+public class SwichCaseExample {
+	
+	int no =3;
+	int check;
+	public void firstswichcase() {
+		
+		switch (check = no) {
+		
+		case 1: System.out.println("tesing first one");
+		
+		case 2: System.out.println("tesing second one");
+		
+		case 3: System.out.println("tesing third one");
+		//break;
+		case 4: System.out.println("tesing fourth one");
+		
+		break;
+		default :
+		
+		System.out.println("tesing five one");
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SwichCaseExample ob = new SwichCaseExample();
+				ob.firstswichcase();
+		
+
+	}
+
+}
