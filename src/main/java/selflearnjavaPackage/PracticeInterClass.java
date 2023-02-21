5939fcb69b9f8c7e0726e304818b6ab28ea2f3ee
arkumar992 <srarunkumar32@gmail.com>
2023-02-21 20:45:01 +0530

    test

--- src/main/java/selflearnjavaPackage/PracticeInterClass.java
+++ src/main/java/selflearnjavaPackage/PracticeInterClass.java
@@ -5,7 +5,7 @@ public class PracticeInterClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		//test
 		TestInterface dr = new ExplainInterfaClass();
 		
 		System.out.println(dr.data());
